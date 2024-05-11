a60cf0129306f9098d6e97b35572c8a5097dabd5
dmoke <dmytromykhaliukk@gmail.com>
2024-05-11 17:28:18 +0300

    feat: io init

--- client/js/sketch.js
+++ client/js/sketch.js
@@ -1,17 +1,134 @@
 var socket;
+var myPlayer;
+var myId;
 
-//before game starts
-function preload(){
-
-}
-//game starts (once)
-function setup(){   
-    socket = io();
-    socket.emit('message', 'HI');
+// everything about the (loading) before the game starts
+function preload() {
+  // write code
 }
 
+// this is the firs thing that is called when the game is started, and it only happens once (Setup)
+function setup() {
+  players = [];
+  myId = 0;
 
-//loop
-function draw(){
-    
+  socket = io();
+
+  socket.emit("imReady", { name: "Devlogerio" });
+
+  socket.on("yourId", function (data) {
+    myId = data.id;
+  });
+
+  socket.on("newPlayer", function (data) {
+    var player = new Player(data.id, data.name, data.x, data.y);
+    players.push(player);
+  });
+
+  socket.on("initPack", function (data) {
+    for (var i in data.initPack) {
+      var player = new Player(
+        data.initPack[i].id,
+        data.initPack[i].name,
+        data.initPack[i].x,
+        data.initPack[i].y
+      );
+      players.push(player);
+      console.log(myId);
+    }
+  });
+
+  socket.on("updatePack", function (data) {
+    for (var i in data.updatePack) {
+      for (var j in players) {
+        if (players[j].id === data.updatePack[i].id) {
+          players[j].location.x = data.updatePack[i].x;
+          players[j].location.y = data.updatePack[i].y;
+          players[j].angle = data.updatePack[i].angle;
+        }
+      }
+    }
+  });
+
+  socket.on("someoneLeft", function (data) {
+    for (var i in players) {
+      if (players[i].id === data.id) {
+        players.splice(i, 1);
+      }
+    }
+  });
+
+  createCanvas(windowWidth, windowHeight);
+}
+
+// this is called alot of times per second (FPS, frame per second)
+function draw() {
+  background(51, 51, 255); // it gets a hex/rgb color
+  sendInputData();
+
+  // TODO optimize this section
+  for (var i in players) {
+    if (players[i].id === myId) {
+      translate(
+        width / 2 - players[i].location.x,
+        height / 2 - players[i].location.y
+      );
+    }
+  }
+
+  fill(51);
+  rect(0, 0, 600, 600);
+
+  for (var i in players) {
+    players[i].draw();
+  }
+}
+
+// The player object constructor
+var Player = function (id, name, x, y) {
+  this.id = id;
+  this.name = name;
+  this.location = createVector(x, y);
+  this.angle = 0;
+
+  this.draw = function () {
+    push();
+    translate(this.location.x, this.location.y);
+    rotate(this.angle);
+    fill(255, 0, 0);
+    beginShape();
+    vertex(30 + 30, 0);
+    vertex(30 + -70, 30);
+    vertex(30 + -45, 0);
+    vertex(30 + -70, -30);
+    endShape(CLOSE);
+    pop();
+
+    // this.speedX = cos(angle) * 3; // cosine is never gonna get more than 1
+    // this.speedY = sin(angle) * 3;
+
+    // if(this.speedX > 3) {
+    //     this.speedX = 3;
+    // }
+
+    // if(this.speedY > 3) {
+    //     this.speedY = 3;
+    // }
+
+    // this.x += this.speedX;
+    // this.y += this.speedY;
+  };
+
+  return this;
+};
+
+function sendInputData() {
+  var angle = atan2(mouseY - windowHeight / 2, mouseX - windowWidth / 2);
+  socket.emit("inputData", {
+    mouseX,
+    mouseY,
+    angle,
+    windowWidth,
+    windowHeight,
+  });
 }
--- server/server.js
+++ server/server.js
@@ -1,22 +1,167 @@
-const { join } = require("path");
-const { createServer } = require("http");
-const express = require("express");
-const socketIO = require("socket.io");
+var path = require("path");
+var http = require("http");
+const favicon = require('serve-favicon');
+var express = require("express");
+var socketIO = require("socket.io");
+var Victor = require("victor");
 
-var client_path = join(__dirname, "../client");
-var port = process.env.port || 3333;
+var publicPath = path.join(__dirname, "../client");
+var port = process.env.PORT || 3333;
 var app = express();
-var server = createServer(app);
+var server = http.createServer(app);
 var io = socketIO(server);
-app.use(express(client_path));
+app.use(express.static(publicPath));
+app.use(favicon(path.join(__dirname, '../favicon.ico')));
+var players = [];
 
 server.listen(port, function () {
-  console.log('Server started on http://localhost:' + port);
+  console.log("Server successfully runned on port http://localhost:" + port); // this will log something to the therminal
 });
 
-io.on('connection', function(socket) {
-  console.log('New connection: ' + socket.id);
-  socket.on('message', function(data) {
-    console.log(data);
+io.on("connection", function (socket) {
+  console.log("someone conencted, Id: " + socket.id);
+  var player = {};
+
+  socket.on("imReady", (data) => {
+    player = new Player(
+      socket.id,
+      data.name,
+      Math.random() * 500,
+      Math.random() * 500
+    );
+    players.push(player);
+
+    socket.emit("yourId", { id: player.id });
+    socket.broadcast.emit("newPlayer", player.getInitPack());
+
+    socket.emit("initPack", { initPack: getAllPlayersInitPack() });
+  });
+
+  socket.on("inputData", (data) => {
+    player.mouseX = data.mouseX;
+    player.mouseY = data.mouseY;
+    player.angle = data.angle;
+    player.windowWidth = data.windowWidth;
+    player.windowHeight = data.windowHeight;
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("someoneLeft", { id: socket.id });
+
+    players = players.filter((element) => element.id !== socket.id);
   });
 });
+
+// The player object constructor
+class Player {
+  constructor(id, name, x, y) {
+    this.id = id;
+    this.name = name;
+    this.location = new Victor(x, y);
+    this.force = new Victor(0, 0);
+    this.acceleration = new Victor(0, 0);
+    this.speed = new Victor(0, 0);
+    this.mouseMaxForce = 0.3;
+    this.maxAcceleration = 3;
+    this.maxSpeed = 3;
+    this.mass = 1;
+    this.mouseX = 0;
+    this.mouseY = 0;
+    this.angle = 0;
+    this.windowWidth = 0;
+    this.windowHeight = 0;
+
+    this.update = function () {
+      this.addForce(this.getForceTwardMouseLocation());
+      this.accelerate();
+      this.speedUp();
+      this.move();
+      this.zeroOutAcceleration();
+    };
+
+    this.getForceTwardMouseLocation = function () {
+      var force = new Victor(
+        this.mouseX - this.windowWidth / 2,
+        this.mouseY - this.windowHeight / 2
+      );
+      force = limitVector(force, this.mouseMaxForce);
+      return force;
+    };
+
+    // amountOfForce is a vector
+    this.addForce = function (amountOfForce) {
+      var force = amountOfForce;
+      this.force = force.divide(new Victor(this.mass, this.mass));
+    };
+
+    this.accelerate = function () {
+      this.acceleration.add(this.force);
+      this.acceleration = limitVector(this.acceleration, this.maxAcceleration);
+    };
+
+    this.speedUp = function () {
+      this.speed.add(this.acceleration);
+      this.speed = limitVector(this.speed, this.maxSpeed);
+    };
+
+    this.move = function () {
+      this.location.add(this.speed);
+    };
+
+    this.zeroOutAcceleration = function () {
+      this.acceleration = new Victor(0, 0);
+    };
+
+    this.getInitPack = function () {
+      return {
+        id: this.id,
+        name: this.name,
+        x: this.location.x,
+        y: this.location.y,
+      };
+    };
+
+    this.getUpdatePack = function () {
+      return {
+        id: this.id,
+        x: this.location.x,
+        y: this.location.y,
+        angle: this.angle,
+      };
+    };
+
+    return this;
+  }
+}
+
+function limitVector(v, max) {
+  var vec = v.clone();
+
+  var mSq = vec.lengthSq();
+  if (mSq > max * max) {
+    vec = vec
+      .divide(new Victor(Math.sqrt(mSq), Math.sqrt(mSq)))
+      .multiply(new Victor(max, max));
+  }
+
+  return vec;
+}
+
+function getAllPlayersInitPack() {
+  var initPack = [];
+  for (var i in players) {
+    initPack.push(players[i].getInitPack());
+  }
+  return initPack;
+}
+
+setInterval(() => {
+  var updatePack = [];
+
+  for (var i in players) {
+    players[i].update();
+    updatePack.push(players[i].getUpdatePack());
+  }
+
+  io.emit("updatePack", { updatePack });
+}, 1000 / 60);
